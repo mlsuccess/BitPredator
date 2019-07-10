988506a24bb3720e251a4ebc34280b21856d8e23
Leonard Melnik <leonard@onedr.io>
2019-07-10 18:25:17 -0400

    fixed bugs
    - added btcOpenDate

--- btcStats.py
+++ btcStats.py
@@ -17,7 +17,16 @@ df["Close"] = new[5]
 df["Volume BTC"] = new[6]
 df["Volume USD"] = new[7]
 y = df.head(100)["Open"]
+x = df.head(100)["Date"]
 y = pd.to_numeric(y)
 btcOpen = pd.to_numeric(df.head(352)["Open"])
-#btcDate = pd.to_numeric(df.head(352)["Date"])
-print(btcOpen)
+btcOpenDate = df.head(352)["Date"]
+plt.plot(x,y,'o')
+#Flip the values so date is going from back to front
+plt.gca().invert_xaxis()
+plt.xlabel("Date")
+plt.ylabel("Open")
+plt.grid()
+
+plt.show()
+print(new[0])
