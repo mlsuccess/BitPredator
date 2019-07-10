cd6b470b589b902778540ae51d041e3ab8de8258
iTecX <30746447+iTecAI@users.noreply.github.com>
2019-07-10 12:48:40 -0400

    WIP trend fetching

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # BitPredator
 Device for automatically conducting the most optimal bitcoin transactions
+
+#Data to read
+- Close
+- Month
+- High
+- Low
+- Temperature
+- Humidity
+- 
--- __init__.py
+++ __init__.py
@@ -1,5 +1,15 @@
 from utils import *
+import time, datetime
 
 df = pd.read_csv('coins.csv')
 
-print(df['Close'].values)
+from pytrends.request import TrendReq
+
+trends = TrendReq(hl='en-US', tz=360)
+trendlist = []
+
+for i in range(1820):
+    start = datetime.today()-datetime.timedelta(days=1820-i)
+    end = datetime.today()-datetime.timedelta(days=1820-i-1)
+    trendlist.append(trends.get_historical_interest(['bitcoin'],day_start=start.days,year_start=start.year))
+
