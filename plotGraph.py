eb5b06d253ba300494794a363f52f97707ad8283
Leonard Melnik <leonard@onedr.io>
2019-07-12 09:01:37 -0400

    Printing more stats when run two graphs

--- plotGraph.py
+++ plotGraph.py
@@ -15,6 +15,7 @@ def graphTwo(x, y, yaxis, xaxis):
     vary = np.var(y)
     covxy = np.mean((x - meanx) * (y - meany))
     rho = covxy / (np.sqrt(varx * vary))
+
     bestY = (meany + rho * (np.sqrt(vary)/np.sqrt(varx))*(x - meanx))
     plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)))
     plt.ylim(min(y)-10,max(y)+10)
@@ -26,3 +27,7 @@ def graphTwo(x, y, yaxis, xaxis):
 
     # Find all the unique points and those shall be the x values, for y it will be th
     plt.show()
+    print("------------------------------------")
+    print("covxy: ", covxy)
+    print("------------------------------------")
+    print("Rho: ", rho)
